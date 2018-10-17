66c69c11d2619be56cb5970e5aa60e7768ef1949
Daniyar Salikhov <salihovdr@gmail.com>
2018-10-17 12:56:54 -0400

    added localStorage persistence

--- src/App.js
+++ src/App.js
@@ -4,80 +4,93 @@ import './grid.css';
 import Column from './components/Column';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state={
-      taskBoard:[]
-    }
-    this.categories = ['This Week', 'Today', 'In Progress', 'Waiting To Discuss', 'Waiting For Feedback', 'Done'];
-  }
+	constructor(){
+		super();
+		this.state={
+			taskBoard:[]
+		};
+		this.categories = ['This Week', 'Today', 'In Progress', 'Waiting To Discuss', 'Waiting For Feedback', 'Done'];
+	}
+  
+	componentDidMount(){
+		this.handleLocalStorage();
+	}
+  
+	handleLocalStorage(){
+		if(!this.state.taskBoard.length){
+			let existingTasks = JSON.parse( localStorage.getItem('taskBoard') );
+			this.setState({
+				taskBoard: existingTasks
+			});
 
-  addToBoard(taskObj){
-    this.setState({
-      taskBoard: [...this.state.taskBoard, taskObj]
-    })
-    // localStorage.setItem(category, JSON.stringify(this.state.taskBoard[category]));
-  }
+		}
+	}
 
-  handleMoveToRight(taskObj){
-    const {id, category} = taskObj;
-    const nextCategoryIndex = this.categories.findIndex(cat=>cat === category)+1;
-    const nextCategory = this.categories[nextCategoryIndex];
-    const updatedTaskList = this.state.taskBoard.map(task => {
-      if (task.id === id){
-        task.category = nextCategory;
-      }
-      return task;
-    });
-    this.setState({
-      taskBoard: updatedTaskList 
-    })
-  }
+	addToBoard(taskObj){
+		this.setState({
+			taskBoard: [...this.state.taskBoard, taskObj]
+		}, () => localStorage.setItem('taskBoard', JSON.stringify(this.state.taskBoard)));
+	}
 
-  handleMoveToLeft(taskObj){
-    const {id, category} = taskObj;
-    const previousCategoryIndex = this.categories.findIndex(cat=>cat === category)-1;
-    const previousCategory = this.categories[previousCategoryIndex];
-    const updatedTaskList = this.state.taskBoard.map(task => {
-      if (task.id === id){
-        task.category = previousCategory;
-      }
-      return task;
-    });
-    this.setState({
-      taskBoard: updatedTaskList 
-    })
-  }
+	handleMoveToRight(taskObj){
+		const {id, category} = taskObj;
+		const nextCategoryIndex = this.categories.findIndex(cat=>cat === category)+1;
+		const nextCategory = this.categories[nextCategoryIndex];
+		const updatedTaskList = this.state.taskBoard.map(task => {
+			if (task.id === id){
+				task.category = nextCategory;
+			}
+			return task;
+		});
+		this.setState({
+			taskBoard: updatedTaskList 
+		}, () => localStorage.setItem('taskBoard', JSON.stringify(this.state.taskBoard)));
+	}
 
-  render() {
-    let columns = this.categories.map((category, i) => {
-      return <Column 
-              key={i}
-              category={category}
-              categories = {this.categories}
-              tasks={this.state.taskBoard } 
-              addToBoard={task => this.addToBoard(task)}
-              moveToRight={task => this.handleMoveToRight(task)}
-              moveToLeft={task => this.handleMoveToLeft(task)}
-            />
-    })
-    return (
-      <div className="App">
-        <div className='container'>
-          <div className='row'>
-            <div className='col-12'>
-              <header className="App-header">
-                <h1>Task Board</h1>
-              </header>
-            </div>
-          </div>
-          <div className='row'>
-            { columns }
-          </div>
-        </div>
-      </div>
-    );
-  }
+	handleMoveToLeft(taskObj){
+		const {id, category} = taskObj;
+		const previousCategoryIndex = this.categories.findIndex(cat=>cat === category)-1;
+		const previousCategory = this.categories[previousCategoryIndex];
+		const updatedTaskList = this.state.taskBoard.map(task => {
+			if (task.id === id){
+				task.category = previousCategory;
+			}
+			return task;
+		});
+		this.setState({
+			taskBoard: updatedTaskList 
+		}, () => localStorage.setItem('taskBoard', JSON.stringify(this.state.taskBoard)));
+	}
+
+	render() {
+		let columns = this.categories.map((category, i) => {
+			return <Column 
+				key={i}
+				category={category}
+				categories = {this.categories}
+				tasks={this.state.taskBoard } 
+				addToBoard={task => this.addToBoard(task)}
+				moveToRight={task => this.handleMoveToRight(task)}
+				moveToLeft={task => this.handleMoveToLeft(task)}
+			/>;
+		});
+		return (
+			<div className="App">
+				<div className='container'>
+					<div className='row'>
+						<div className='col-12'>
+							<header className="App-header">
+								<h1>Task Board</h1>
+							</header>
+						</div>
+					</div>
+					<div className='row'>
+						{ columns }
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,62 +2,62 @@ import React from 'react';
 import uuidv4 from 'uuid/v4';
 
 export default class TaskForm extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      task: '',
-      creating: false
-    }
-  }
+	constructor(){
+		super();
+		this.state={
+			task: '',
+			creating: false
+		};
+	}
 
-  handleSubmit(e){
-    e.preventDefault();
-    e.target.reset();
-    this.setState({
-      creating: false
-    })
-    let id = uuidv4();
-    let task = this.state.task
-    this.props.sendTask(task, id);
-  }
+	handleSubmit(e){
+		e.preventDefault();
+		e.target.reset();
+		this.setState({
+			creating: false
+		});
+		let id = uuidv4();
+		let task = this.state.task;
+		this.props.sendTask(task, id);
+	}
 
-  handleChange(e){
-    this.setState({
-      task: e.target.value,
-    })
-  }
+	handleChange(e){
+		this.setState({
+			task: e.target.value,
+		});
+	}
 
-  toggleAddBtn(){
-    this.setState({
-      creating: true
-    })
-  }
+	toggleAddBtn(){
+		this.setState({
+			creating: true
+		});
+	}
 
-  render(){
+	render(){
 
-    let form;
-    let addBtn;
+		let form;
+		let addBtn;
 
-    if (!this.state.creating){
-      addBtn = (<button onClick={()=>this.toggleAddBtn()}>New</button>)
-      form = '';
-    } else {
-      addBtn='';
-      form = (<form onSubmit={(e)=> this.handleSubmit(e)}>
-                  <input 
-                    type='text'
-                    value={this.state.value}
-                    onChange={(e)=>this.handleChange(e)}
-                  />
-                  <input type='submit'/>
-                </form>);
-    }
+		if (!this.state.creating){
+			addBtn = (<button onClick={()=>this.toggleAddBtn()}>New</button>);
+			form = '';
+		} else {
+			addBtn='';
+			form = (<form onSubmit={(e)=> this.handleSubmit(e)}>
+				<input 
+					type='text'
+					value={this.state.value}
+					onChange={(e)=>this.handleChange(e)}
+				/>
+				<input type='submit'/>
+			</form>);
+		}
     
-    return (
-      <React.Fragment>
-        { form }
-        { addBtn }
-      </React.Fragment>
-    );
-  }
+		return (
+			<React.Fragment>
+				{ form }
+				{ addBtn }
+			</React.Fragment>
+		);
+	}
 }
